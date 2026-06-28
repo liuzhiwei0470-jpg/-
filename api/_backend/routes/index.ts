a1f51404da30dfafaafd185f045c31203b5104ba
刘智伟 <liuzhiwei@Mac-Liu.local>
2026-06-28 21:04:04 +0800

    fix: 改用普通import方式导出路由

--- api/_backend/routes/index.ts
+++ api/_backend/routes/index.ts
@@ -1,7 +1,17 @@
-export { default as authRoutes } from './auth.routes';
-export { default as subscriptionRoutes } from './subscription.routes';
-export { default as categoryRoutes } from './category.routes';
-export { default as healthRoutes } from './health.routes';
-export { default as articleRoutes } from './article.routes';
-export { default as settingsRoutes } from './settings.routes';
-export { default as shareRoutes } from './share.routes';
+import authRoutes from './auth.routes';
+import subscriptionRoutes from './subscription.routes';
+import categoryRoutes from './category.routes';
+import healthRoutes from './health.routes';
+import articleRoutes from './article.routes';
+import settingsRoutes from './settings.routes';
+import shareRoutes from './share.routes';
+
+export {
+  authRoutes,
+  subscriptionRoutes,
+  categoryRoutes,
+  healthRoutes,
+  articleRoutes,
+  settingsRoutes,
+  shareRoutes,
+};
