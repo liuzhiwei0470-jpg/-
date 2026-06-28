91aba70d13a2a9ef9ce3387c48d0c98d4ddd3664
刘智伟 <liuzhiwei@Mac-Liu.local>
2026-06-28 21:00:22 +0800

    test: 添加cors和helmet中间件

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,10 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
 app.get('/test', (req, res) => {
-  res.json({ message: 'Express works!' });
+  res.json({ message: 'Express with middleware works!' });
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
